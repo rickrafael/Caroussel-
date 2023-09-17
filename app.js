a14ff15c6c190438f5a2fda165cdfe676d084a0d
Ricardo Rafael <130177490+rickrafael@users.noreply.github.com>
2023-09-17 18:55:09 -0300

    codes

---
+++ app.js
@@ -0,0 +1,37 @@
+//Obtendo referências
+const slides = document.querySelectorAll('[data-js="caroussel_igm"]')
+const nextBtn = document.querySelector('[data-js="next_button"]')
+const prevBtn = document.querySelector('[data-js="prev_button"]')
+
+//variável universal Counter
+let correctSlideIndex = 0
+
+//adicionando Listener de evento
+nextBtn.addEventListener('click', () => {
+  if(correctSlideIndex === slides.length-1){
+    correctSlideIndex = 0
+  }else{
+    correctSlideIndex++
+  }
+
+  slides.forEach((slide) => {
+     slide.classList.remove('img_visible')
+  })
+
+  slides[correctSlideIndex].classList.add('img_visible')
+})
+
+prevBtn.addEventListener('click', () => {
+  if(correctSlideIndex === 0){
+    correctSlideIndex = slides.length-1
+  }else{
+    correctSlideIndex--
+  }
+
+  slides.forEach((slide) => {
+   slide.classList.remove('img_visible')
+  })
+
+  slides[correctSlideIndex].classList.add('img_visible')
+})
+
